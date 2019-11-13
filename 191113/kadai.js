5bcb9ac4f5832e3208eacc13def1b9ba8b69e29a
HijikiRice <m0118052cf@edu.teu.ac.jp>
2019-11-13 18:12:02 +0900

    第5回授業

--- 191113/kadai.js
+++ 191113/kadai.js
@@ -1,15 +1,40 @@
 function checkAge() {
   let userInput = document.querySelector("#age").value;
   let result;
-  if(0 < userInput&&userInput < 20){
+
+  if((age) < 20 && Number(age) > 0){
     result = "未成年";
-  }else if(20 <= userInput&&userInput < 65){
+  }else if(Number(age) >= 20 && Number(age) < 65){
     result = "成人";
-  }else if(65 <= userInput){
+  }else if(Number(age) >= 65){
     result = "高齢者";
-  }else if(userInput <= 0){
+  }else if(Number(age) <= 0){
     result = "エラー";
+  }
+
+  let answerElement = document.querySelector("#answer");
+  answerElement.innerHTML = result;
+}
+
+
+
+/*
+/記述例
+
+function checkAge() {
+  let age = document.querySelector("#age").value;
+  let result;
+  // ここにif文を書いて、表示させたい文字列をresultに代入する
+  if(Number(age) <= 0){
+    result = "エラー";
+  }else if(Number(age) < 20){
+    result = "未成年";
+  }else if(Number(age) < 65){
+    result = "成人";
+  }else{
+    result = "高齢者";
   }
   let answerElement = document.querySelector("#answer");
   answerElement.innerHTML = result;
 }
+*/
