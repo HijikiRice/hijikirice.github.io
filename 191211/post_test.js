1a7f9efb61133598bd21ed3a7d95edf86ea3424b
HijikiRice <m0118052cf@edu.teu.ac.jp>
2019-12-18 18:01:33 +0900

    第10回授業

--- 191211/post_test.js
+++ 191211/post_test.js
@@ -1,13 +1,22 @@
-fetch("http://zipcloud.ibsnet.co.jp/api/search?zipcode=1920916").then(
-    function (response) {
-      return response.json();
-    }
-).then(
-    function (json) {
-      let answerHtml = "";
-      json.forEach(function (tweet) {
-        answerHtml += `<li>${tweet.name}</li>`;
-      });
-      document.querySelector("#answer").innerHTML = answerHtml;
-    }
-);
+function zipSearch() {
+  let zip = document.querySelector("#zip").value;
+  console.log(zip)
+  fetch("https://api.zipaddress.net/?zipcode=" + zip).then(
+      function (res) {
+        console.log(res)
+        return res.json();
+      }
+  ).then(
+      function (json) {
+        console.log(json)
+        let answerHtml = "";
+
+        if(json.code == 200){
+        answerHtml += json.data.fullAddress;
+        }else{
+          answerHtml += "存在しない郵便番号です";
+        }
+        document.querySelector("#answer").innerHTML = answerHtml;
+      }
+  );
+}
